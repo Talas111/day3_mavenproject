f609e75ab208a6aa485295be8c2f3c7b30f24d31
Shirin <shirkopai@gmail.com>
2021-05-26 18:24:30 -0400

    new file coming to work! for next whole week project

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,3 +1,7 @@
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world ");
+        System.out.println("Akbar talks way too fast! that nobody can catch up and we have to re watch");
+    }
 
 }
