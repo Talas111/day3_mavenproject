910c9ba6dfebeaba7b38077efbc8d588222f1065
Shirin <shirkopai@gmail.com>
2021-05-27 09:39:55 -0400

    info about life

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -10,7 +10,17 @@ public class Hello {
 
 
         // work one for now
-        
+        //lets say second commit for now
+
+
+
+        //funny stuff going aroiound here  looks like
+        // need to commit alll those changes every time
+
+        System.out.println("Today is May 27 , tomorrow my daughters birthday! ");
+        System.out.println("I love my her so much! hope My kids will be happy! healthy! and they will have long live!");
+
+
     }
 
 }
